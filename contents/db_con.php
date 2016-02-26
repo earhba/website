b6fc5af78b79e92350e1b693beffd606f1e344e5
Hasitha Athauda <earhba@gmail.com>
2016-02-26 09:54:29 +0530

    ok
    ok

---
+++ contents/db_con.php
@@ -0,0 +1,12 @@
+<?php
+
+$con = mysql_connect("localhost", "root","");
+	
+	if(!$con){
+		
+		die ('could not connect :' . mysql_error());
+	}
+	
+	mysql_select_db("glassware", $con);
+	
+	?>
---
+++ contents/nav.php
@@ -0,0 +1,10 @@
+
+	 
+      <ul>
+        <li class="last"><a href="pages/gallery.html">Contact Us</a></li>
+		<li ><a href="pages/gallery.php">About</a></li>
+		<li ><a href="pages/gallery.php">Gallery</a></li>
+        <li class="active"><a href="index.html">Homepage</a></li>
+      </ul>
+    
+
